da7018b9a58516ac4061e841190db0f2172646ce
lezgievlela278 <lezgievlela278@gmail.com>
2025-04-01 14:21:33 +0000

    Add 20.c

---
+++ src/20.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int x = 5;
+    if (x > 10) {
+        printf("The value of x is greater than 10.\n");
+    }
+    else {
+        printf("The value of x is not greater than 10.\n");
+    }
+
+    return 0;
+}
