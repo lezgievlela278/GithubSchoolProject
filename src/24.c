2bd3d004618d359a6cf43da4f2b607c8029a8bc4
lezgievlela278 <lezgievlela278@gmail.com>
2025-04-06 06:09:03 +0000

    Add 24.c

---
+++ src/24.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int a = 5;
+    int b = 10;
+
+    if (a > b) {
+        printf("A is greater than B\n");
+    } else {
+        printf("B is greater than A\n");
+    }
+
+    return 0;
+}
